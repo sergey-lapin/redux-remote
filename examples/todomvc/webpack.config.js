59513c32976e9cfd42f2af1f83c9f9330786eb4f
Sergey Lapin <lapanoid@gmail.com>
2015-07-24 10:59:10 +0300

    return standard port

--- examples/todomvc/webpack.config.js
+++ examples/todomvc/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:3001',
+    'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
     './index'
   ],
